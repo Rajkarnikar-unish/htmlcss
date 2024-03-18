9399ddba64818f05498715e737bf4e9fc9af9f67
Unish Rajkarnikar <ubidy947@gmail.com>
2024-03-17 21:42:03 -0500

    DOM Manipulation & Event Handling

--- dom-manipulation/README.md
+++ dom-manipulation/README.md
@@ -1,6 +1,6 @@
 DOM Manipulation
 
-1. What the DOM is?
+1. What the DOM is?\n
    <strong>DOM</strong> stands for Document Object Model. We use it to manipulate content style, and structure.
    One of the most useful tools of JavaScript.
 2. Methods to Select Elements in the DOM
--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -1,5 +1,41 @@
 // DOM Manipulation
 
+//EVENT LISTENERS
+
+const revealBtn = document.querySelector(".reveal-btn");
+
+const hiddenContent = document.querySelector('.hiddent-content');
+
+function revealContent() {
+    if (hiddenContent.classList.contains('reveal-btn')) {
+        hiddenContent.classList.remove('reveal-btn');
+    } else {
+        hiddenContent.classList.add('reveal-btn');
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
+
+//element.addEventListeners("click", function)
+
+// const buttonTwo = document.querySelector(".btn-2");
+
+// function alertBtn() {
+//     alert("This is really cool.");
+// };
+
+// buttonTwo.addEventListener("click", alertBtn);
+
+// //Mouse Over (Hover)
+
+// const newBgColor = document.querySelector('.box-3');
+
+// function changeBgColor() {
+//     newBgColor.style.backgroundColor = 'blue';
+// }
+
+// newBgColor.addEventListener("mouseover", changeBgColor);
+
 // const heading = document.querySelector('#main-heading');
 
 // heading.style.color = 'red';
@@ -17,27 +53,68 @@
 // console.log(listItems);
 
 //Creating elements
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
 
-//Adding elements 
-ul.append(li);
+// //Adding elements
+// ul.append(li);
 
-//Modifying texts
+// //Modifying texts
 
-li.innerText = 'X-Men';
+// li.innerText = 'X-Men';
 
-const firstListItem = document.querySelector('.list-items');
+// const firstListItem = document.querySelector('.list-items');
 
-console.log(firstListItem.innerText);
-console.log(firstListItem.textContent);
-console.log(firstListItem.innerHTML);
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.textContent);
+// console.log(firstListItem.innerHTML);
 
-//Modifying attributes & classes
-li.setAttribute('id', 'main-heading')
-//li.className = 'list-items';
-li.removeAttribute('id');
+// //Modifying attributes & classes
 
-const title = document.querySelector('#main-heading');
+// li.classList.add('list-items')
+// // li.classList.remove('list-items')
 
-console.log(title.getAttribute('id'));
+// console.log(li.classList.contains('list-items'));
+// li.setAttribute('id', 'main-heading')
+// //li.className = 'list-items';
+// li.removeAttribute('id');
+
+// const title = document.querySelector('#main-heading');
+
+// console.log(title.getAttribute('id'));
+
+//REMOVE ELEMENTS
+
+// li.remove();
+
+
+//Traverse the DOM
+
+//Parent Node Traversal
+
+// console.log(ul);
+
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+// const html = document.documentElement;
+
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+//Child Node Traversal
+
+// console.log(ul.childNodes);
+
+// ul.childNodes[1].style.backgroundColor = 'blue';
+
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+//Sibling Node Traversal
+// let ul = document.querySelector('ul');
+
+
+// console.log(ul.previousElementSibling);
+// console.log(ul.nextElementSibling);
