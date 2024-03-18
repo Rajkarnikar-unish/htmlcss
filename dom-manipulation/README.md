0805654da71c502c9f6e872e543cdddb293dcdf5
Unish Rajkarnikar <ubidy947@gmail.com>
2024-03-18 16:33:58 -0500

    event propogation & delegation

--- dom-manipulation/README.md
+++ dom-manipulation/README.md
@@ -1,9 +1,19 @@
 DOM Manipulation
 
-1. What the DOM is?\n
+1. What the DOM is?
    <strong>DOM</strong> stands for Document Object Model. We use it to manipulate content style, and structure.
    One of the most useful tools of JavaScript.
 2. Methods to Select Elements in the DOM
 3. How to Traverse the DOM
 4. How to Manipulate the DOM
 5. Event Handling
+
+   Event Propagation
+   This means how an event travels through the DOM. Event propogation cover 3 terms:
+
+   1. Event Capturing
+   2. Target
+   3. Event Bubbling
+
+   Event Delegation
+   It allows users to append a Single event listener to a parent element that adds it to all of its present and future descendants that match a selector.
--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -2,119 +2,24 @@
 
 //EVENT LISTENERS
 
-const revealBtn = document.querySelector(".reveal-btn");
+document.querySelector('#sports').addEventListener
+    ('click', function (e) {
+        console.log(e.target.getAttribute('id') + ' is clicked');
 
-const hiddenContent = document.querySelector('.hiddent-content');
+        const target = e.target;
 
-function revealContent() {
-    if (hiddenContent.classList.contains('reveal-btn')) {
-        hiddenContent.classList.remove('reveal-btn');
-    } else {
-        hiddenContent.classList.add('reveal-btn');
-    }
-}
+        let clicked = false;
 
-revealBtn.addEventListener('click', revealContent);
+        if (target.matches('li')) {
+            target.style.textDecoration = 'line-through';
+        } else {
+            target.style.backgroundColor = 'transparent';
+        }
+    });
 
-//element.addEventListeners("click", function)
+const sports = document.querySelector('#sports');
+const newSport = document.createElement('li');
+newSport.innerText = 'Rugby';
+newSport.setAttribute('id', 'rugby');
 
-// const buttonTwo = document.querySelector(".btn-2");
-
-// function alertBtn() {
-//     alert("This is really cool.");
-// };
-
-// buttonTwo.addEventListener("click", alertBtn);
-
-// //Mouse Over (Hover)
-
-// const newBgColor = document.querySelector('.box-3');
-
-// function changeBgColor() {
-//     newBgColor.style.backgroundColor = 'blue';
-// }
-
-// newBgColor.addEventListener("mouseover", changeBgColor);
-
-// const heading = document.querySelector('#main-heading');
-
-// heading.style.color = 'red';
-
-// console.log(heading);
-
-// const listItems = document.querySelectorAll('.list-items');
-
-// // listItems.style.fontSize = '40px';
-
-// for (i = 0; i < listItems.length; i++) {
-//     listItems[i].style.fontSize = '4rem';
-// }
-
-// console.log(listItems);
-
-//Creating elements
-// const ul = document.querySelector('ul');
-// const li = document.createElement('li');
-
-// //Adding elements
-// ul.append(li);
-
-// //Modifying texts
-
-// li.innerText = 'X-Men';
-
-// const firstListItem = document.querySelector('.list-items');
-
-// console.log(firstListItem.innerText);
-// console.log(firstListItem.textContent);
-// console.log(firstListItem.innerHTML);
-
-// //Modifying attributes & classes
-
-// li.classList.add('list-items')
-// // li.classList.remove('list-items')
-
-// console.log(li.classList.contains('list-items'));
-// li.setAttribute('id', 'main-heading')
-// //li.className = 'list-items';
-// li.removeAttribute('id');
-
-// const title = document.querySelector('#main-heading');
-
-// console.log(title.getAttribute('id'));
-
-//REMOVE ELEMENTS
-
-// li.remove();
-
-
-//Traverse the DOM
-
-//Parent Node Traversal
-
-// console.log(ul);
-
-// console.log(ul.parentNode.parentNode);
-// console.log(ul.parentElement.parentElement);
-
-// const html = document.documentElement;
-
-// console.log(html.parentNode);
-// console.log(html.parentElement);
-
-//Child Node Traversal
-
-// console.log(ul.childNodes);
-
-// ul.childNodes[1].style.backgroundColor = 'blue';
-
-// console.log(ul.children);
-// console.log(ul.firstElementChild);
-// console.log(ul.lastElementChild);
-
-//Sibling Node Traversal
-// let ul = document.querySelector('ul');
-
-
-// console.log(ul.previousElementSibling);
-// console.log(ul.nextElementSibling);
+sports.appendChild(newSport);
